bd42eb04f1a4080c657087c2a7b4807c9d5defd1
ikhushibisht <khushibisht2319@gmail.com>
2023-11-27 10:25:56 +0530

    login form

--- lib/src/features/authentication/screens/login/login_form.dart
+++ lib/src/features/authentication/screens/login/login_form.dart
@@ -3,16 +3,36 @@ import 'package:parvaah_helping_hand/src/constants/sizes.dart';
 import 'package:parvaah_helping_hand/src/constants/text_string.dart';
 import 'package:parvaah_helping_hand/src/features/authentication/screens/forgot_password/forgot_options/forgot_pass_model.dart';
 
-class LoginForm extends StatelessWidget {
-  const LoginForm({
-    Key? key,
-  }) : super(key: key);
+class LoginForm extends StatefulWidget {
+  const LoginForm({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  bool _isPasswordVisible = false;
+  late TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     var mediaQuery = MediaQuery.of(context);
     var brightness = mediaQuery.platformBrightness;
     final isDarkMode = brightness == Brightness.dark;
+
     return Form(
       child: Container(
         padding: const EdgeInsets.symmetric(vertical: tFormHeight - 30),
@@ -28,14 +48,24 @@ class LoginForm extends StatelessWidget {
             ),
             const SizedBox(height: tFormHeight - 45),
             TextFormField(
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.fingerprint),
+              obscureText: !_isPasswordVisible,
+              controller: _passwordController,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.fingerprint),
                 labelText: tPassword,
                 hintText: tPassword,
-                border: OutlineInputBorder(),
+                border: const OutlineInputBorder(),
                 suffixIcon: IconButton(
-                  onPressed: null,
-                  icon: Icon(Icons.remove_red_eye_sharp),
+                  onPressed: () {
+                    setState(() {
+                      _isPasswordVisible = !_isPasswordVisible;
+                    });
+                  },
+                  icon: Icon(
+                    _isPasswordVisible
+                        ? Icons.visibility_off
+                        : Icons.remove_red_eye_outlined,
+                  ),
                 ),
               ),
             ),
@@ -53,7 +83,9 @@ class LoginForm extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  // Access password using _passwordController.text
+                },
                 child: const Text(tLogin),
               ),
             ),
